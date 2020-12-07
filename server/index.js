e598b3b24263c50acb6c5a71fbb40ecb8dcdab04
ashwin7mak <m.ashwinkumar7@gmail.com>
2020-12-07 09:57:27 -0800

    Updated graphql query to query using parameter in portfolio type

--- server/index.js
+++ server/index.js
@@ -66,7 +66,7 @@ app.prepare()
       }
       type Query {
         hello: String
-        portfolio: Portfolio
+        portfolio(id: ID): Portfolio
         portfolios: [Portfolio]
       }
     `);
@@ -76,8 +76,9 @@ app.prepare()
       hello: () => {
         return 'Hello world!';
       },
-      portfolio: () => {
-        return data.portfolios[0];
+      portfolio: ({ id }) => {
+        const portfolio = data.portfolios.find(e => e._id === id);
+        return portfolio;
       },
       portfolios: () => {
         return data.portfolios;
