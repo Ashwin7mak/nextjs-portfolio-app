c109a5964ed8c52a5a408f2c7518dfc5a5b1bb8d
ashwin7mak <m.ashwinkumar7@gmail.com>
2020-12-08 21:20:41 -0800

    Added query by id in the portfolioDetail component

--- pages/portfolios/[id].js
+++ pages/portfolios/[id].js
@@ -1,13 +1,78 @@
 import { useRouter } from "next/router";
 
-const PortfolioDetail = () => {
+import axios from 'axios';
 
-  const router = useRouter();
-  const id = router.query.id;
+const fetchPortfolioById = (id) => {
+  const query = `
+    query Portfolio {
+      portfolio(id: "${id}") {
+        _id
+        company
+        location
+        content
+        jobTitle
+        experience
+        startDate
+        endDate
+        isCurrentlyEmployed
+      }
+    }`;
+
+  const url = 'http://localhost:3000/graphql';
+
+  return axios.post(url, { query })
+    .then(({data: graph}) => graph.data)
+    .then(data => data.portfolio);
+}
+
+const PortfolioDetail = ({portfolio}) => {
 
   return (
-    <h1>I am visiting the ID: {id}</h1>
+    <>
+    <div className="portfolio-detail">
+    `` <div className="container">
+
+        <div className="jumbotron">
+          <h1 className="display-3">{portfolio.company}</h1>
+          <p className="lead">{portfolio.jobTitle}</p>
+          <p>
+            <a className="btn btn-lg btn-success" href="#" role="button">
+              See Company</a>
+            </p>
+        </div>
+
+        <div className="row marketing">
+          <div className="col-lg-6">
+            <h4 className="title">Location</h4>
+            <p className="text">{portfolio.location}</p>
+
+            <h4 className="title">Start Date</h4>
+            <p className="text">{portfolio.startDate}</p>
+          </div>
+
+          <div className="col-lg-6">
+            {/* TODO: days later... */}
+            <h4 className="title">Days</h4>
+            <p className="text">44</p>
+
+            <h4 className="title">End Date</h4>
+            <p className="text">{portfolio.endDate}</p>
+          </div>
+          <div className="col-md-12">
+            <hr />
+            <h4 className="title">Description</h4>
+              <p>{portfolio.content}</p>
+            </div>
+        </div>
+      </div>
+    </div>
+    </>
   )
+}
+
+PortfolioDetail.getInitialProps = async ({query}) => {
+  const portfolio = await fetchPortfolioById(query.id);
+  return { portfolio };
 }
 
 export default PortfolioDetail;
